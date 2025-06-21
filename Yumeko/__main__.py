ac4c9b407208f510cde7ee77139348b1e376abcd
slayer <meerutuniversity996@gmail.com>
2025-06-21 14:58:17 +0530

    Update __main__.py

--- Yumeko/__main__.py
+++ Yumeko/__main__.py
@@ -224,7 +224,7 @@ async def handle_main_menu_callback(client, query: CallbackQuery):
 @app.on_message(filters.command(["start" , "help"], prefixes=config.COMMAND_PREFIXES) & filters.group)
 async def start_command(client, message: Message):
     button = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Sᴛᴀʀᴛ ɪɴ ᴘᴍ", url="https://t.me/Frierenzbot?start=help")]
+        [InlineKeyboardButton("Sᴛᴀʀᴛ ɪɴ ᴘᴍ", url="https://t.me/shigaraki_probot?start=help")]
     ])
     await message.reply(
         text=f"**𝖧𝖾𝗅𝗅𝗈, {message.from_user.first_name} <3**\n"
