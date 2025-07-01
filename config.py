530b18a86ea40fd62205e54d402951d3d9ab2a3d
slayer <meerutuniversity996@gmail.com>
2025-07-01 11:16:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class config:
     #Client
     API_ID = 26249286
     API_HASH = "4e3bf0b014fda4ac752e8f4ab854279b"
-    BOT_TOKEN = "7979900579:AAEnuFbCs58CMA8qrEIgIyvC4VEc0ZXUOco"
+    BOT_TOKEN = "7979900579:AAFYIZ0SEhb9z2CtVgb_tnPSwhCZWeaYJ_I"
     BOT_NAME = "sʜɪɢᴀʀᴀkɪ ᴛᴏᴍᴜʀᴀ"
     BOT_USERNAME = "shigaraki_probot"
     BOT_ID = 7979900579
